1895d5f7769938d23c1d563fb3c760082f6f3267
Katagiri <katagiri@plala.co.jp>
2015-09-29 16:34:07 +0900

    Initial Commit

---
+++ LocalNotifyUtility/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotifyUtility
+//
+//  Created by 片桐奏羽 on 2015/09/29.
+//  Copyright (c) 2015年 SoKatagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
