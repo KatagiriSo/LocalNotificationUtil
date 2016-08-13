065c671704a86385384e8180cf50b11f58b9b1fe
KatagiriSo <katagiri@rodhos.info>
2016-08-13 20:49:24 +0900

    add timePicker

--- LocalNotificationUtil.swift
+++ LocalNotificationUtil.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class LocalNotificationUtil: NSObject {
-   
+
     var types = UIUserNotificationType.Alert
 
-    func setUpTime(notification:UILocalNotification) -> UILocalNotification
+    func setUpTime(notification:UILocalNotification, date:NSDate?) -> UILocalNotification
     {
         // time
-        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
-        notification.timeZone = NSTimeZone.defaultTimeZone()
-//        notification.repeatCalendar
-//        notification.repeatInterval
+        let date = {()->NSDate in
+            if (date != nil) {
+                return date!
+            } else {
+                return NSDate(timeIntervalSinceNow: 5)
+            }
+        }()
+        
+        let calender = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let timezone = NSTimeZone.localTimeZone()
+        let repeatInterval : NSCalendarUnit = .Day
+
+        notification.fireDate = date
+        notification.timeZone = timezone
+        notification.repeatCalendar = calender
+        notification.repeatInterval = repeatInterval
+
         
         return notification
     }
@@ -33,17 +46,35 @@ class LocalNotificationUtil: NSObject {
     }
     
 
-    func getSampleNotification()->UILocalNotification {
+    func getSampleNotification(time:NSDate)->UILocalNotification {
         print("setNotification")
         
         let setting = UIUserNotificationSettings(forTypes: types, categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(setting)
         
-        return setUpAlert(setUpTime(UILocalNotification()))
+        let notification = UILocalNotification()
+        setUpTime(notification, date: time)
+        setUpAlert(notification)
         
+        return notification
     }
     
-    func scheduleSampleNotification(){
-        UIApplication.sharedApplication().scheduleLocalNotification(getSampleNotification())
+    func scheduleSampleNotification(time:NSDate){
+        
+        let notification = getSampleNotification(time)
+        
+        let app = UIApplication.sharedApplication()
+        app.scheduleLocalNotification(notification)
     }
+    
+    func clear() {
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+    }
+    
+    func settingNotifications()->[UILocalNotification]?
+    {
+        return UIApplication.sharedApplication().scheduledLocalNotifications
+    }
+    
+    
 }
--- LocalNotifyUtility/ViewController.swift
+++ LocalNotifyUtility/ViewController.swift
@@ -9,11 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var timeLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateTimeLabel()
+
     }
+    
+    @IBAction func timePickerValueChanged(sender: AnyObject) {
+        print("timePickerValueChanged")
+    }
+    
+    @IBAction func timePickerLongPressed(sender: AnyObject) {
+        print("timePickerLongPressed")
+        
+        let date = self.timePicker.date;
+        
+        let notifiUtil = LocalNotificationUtil()
+        notifiUtil.clear()
+        notifiUtil.scheduleSampleNotification(date)
+        
+        let vc = UIAlertController(title: "time", message: "local set\(date)", preferredStyle: .Alert)
+       
+        let ac = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) in
+            print("close")
+        }
+        
+        vc.addAction(ac)
+        
+        self.presentViewController(vc, animated: true, completion: {
+            self.updateTimeLabel()
+        })
+    }
+    
+    
+    func updateTimeLabel() {
+        let notificationUtil = LocalNotificationUtil()
+        let us_ = notificationUtil.settingNotifications()
+        
+        if us_ != nil {
+            let us = us_!
+            if us.count != 0 {
+                let n = us[0]
+                self.timeLabel.text = n.fireDate?.description
+            }
+        }
+  
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,8 +70,7 @@ class ViewController: UIViewController {
     }
 
     override func viewWillAppear(animated: Bool) {
-        let notifiUtil = LocalNotificationUtil()
-        notifiUtil.scheduleSampleNotification()
+
     }
 
 }
